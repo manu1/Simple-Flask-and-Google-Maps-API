300cace84f9ba2e72e3de26537b90ffe9be8ff34
larryeitel <larry@eitel.com>
2013-02-17 15:47:32 -0600

    google maps api start

--- routes.py
+++ routes.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
 
+@app.route('/map')
+def map():
+  return render_template('map.html')
+
 @app.route('/about')
 def about():
   return render_template('about.html')
